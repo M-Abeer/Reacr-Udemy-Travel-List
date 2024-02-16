851ab0020d25b28c45f9bb93b5f5092533050be5
M-Abeer <102969595+M-Abeer@users.noreply.github.com>
2024-02-17 00:19:46 +0500

    far away

--- travellist/src/App.js
+++ travellist/src/App.js
@@ -1,11 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
-
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+];
 const App = () => {
   return (
-    <div>
-      <h1>A❤N</h1>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 };
+
+const Logo = () => <h1>🌴Far Away👜</h1>;
+const Form = () => {
+  const [description, setDescription] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert("hello");
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your😍 trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        placeholder="Items..."
+        type="text"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>ADD</button>
+    </form>
+  );
+};
+const PackingList = () => {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+const Item = ({ item }) => {
+  const { id, description, quantity, packed } = item;
+  console.log(id, description, quantity, packed);
+  return (
+    <li>
+      <span style={packed ? { textDecoration: "line-through" } : {}}>
+        {quantity} {description}
+      </span>
+
+      <button>❌</button>
+    </li>
+  );
+};
+const Stats = () => {
+  return (
+    <footer>You have X items on your list,and you already packed X (X%)</footer>
+  );
+};
+
 export default App;
